d8ed3080df001f3c3450ab4fc29062279b89b908
John Arthorne <john.arthorne@gmail.com>
2026-02-19 16:31:52 -0500

    Add models to admin console

--- core/admin.py
+++ core/admin.py
@@ -1,4 +1,26 @@
 from django.contrib import admin
-from .models import MonthlyGenerationData
+from .models import MonthlyGenerationData, Country, Fuel, CountryFuel
 
-admin.site.register(MonthlyGenerationData)
+@admin.register(MonthlyGenerationData)
+class MonthlyGenerationDataAdmin(admin.ModelAdmin):
+    list_display = ('country', 'fuel_type', 'date', 'generation_twh', 'share_of_generation_pct')
+    list_filter = ('country', 'fuel_type', 'date')
+    search_fields = ('country', 'country_code', 'fuel_type')
+
+@admin.register(Country)
+class CountryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'code', 'electricity_rank', 'generation_latest_12_months')
+    search_fields = ('name', 'code')
+    ordering = ('electricity_rank',)
+
+@admin.register(Fuel)
+class FuelAdmin(admin.ModelAdmin):
+    list_display = ('type', 'rank')
+    search_fields = ('type',)
+    ordering = ('rank',)
+
+@admin.register(CountryFuel)
+class CountryFuelAdmin(admin.ModelAdmin):
+    list_display = ('country', 'fuel', 'share', 'generation_latest_12_months')
+    list_filter = ('fuel',)
+    search_fields = ('country__name', 'country__code', 'fuel__type')
